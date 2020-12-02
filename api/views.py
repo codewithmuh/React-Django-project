8d40b37eb0722aa9b660684c5a6bbe03f3e4eb81
eagle7 <khizar_siddique@yahoo.com>
2020-12-02 17:17:55 +0500

    D

--- api/views.py
+++ api/views.py
@@ -5,10 +5,31 @@ import json
 from decouple import config, Csv
 
 
+
+def getToken(request):
+    if request.GET.get('code') and request.GET.get('context') and request.GET.get('scope'):
+        code = request.GET.get('code')
+        context = request.GET.get('context')
+        scope = request.GET.get('scope')
+        redirect = 'https://bigcommerce02.herokuapp.com/cb/auth'
+        
+        store_hash = context.split('/')[1]
+
+        client = BigcommerceApi(client_id=client_id(), store_hash=config('apiStoreHash'))
+
+        token = client.oauth_fetch_token(client_secret(), code, context, scope, redirect)
+        bc_user_id = token['user']['id']
+        email = token['user']['email']
+        access_token = token['access_token']
+        return access_token
+
+
 def orders(request):
 
+    access_token = getToken(request)
+    print(access_token)
     url = 'https://api.bigcommerce.com/stores/4zjutairi8/v2/orders'
-    headers = {'X-Auth-Token':  config('apiToken') , 'Accept': 'application/json', 'host':'api.bigcommerce.com'  ,'Content-Type': 'application/json'}
+    headers = {'X-Auth-Token':  access_token , 'Accept': 'application/json', 'host':'api.bigcommerce.com'  ,'Content-Type': 'application/json'}
     r = requests.get(url, headers=headers)
     return HttpResponse(r)
 
