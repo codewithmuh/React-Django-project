5285a75c3b565dd60bb2171bba49c5369f27fd67
eagle7 <khizar_siddique@yahoo.com>
2020-12-04 15:36:35 +0500

    F

--- Readme.md
+++ Readme.md
@@ -14,7 +14,9 @@ apiToken = ewbkmr7y7nicrvttwidmad2j4odnr72
 apiStoreHash = 4zjutairi8
 appClientId = bayqxi2klatn69zwxd28jga88p71zpb
 appClientSecret = f1278160de8105d9ddda21869193952af82548d2e95ddcb9cb6eabda167271c1
-
+callBackURL =
+DISABLE_COLLECTSTATIC=1
+DEBUG_COLLECTSTATIC = 1
 Open URL https://dashboard.heroku.com/apps
 Create a new App
 Select Deployment Method GIthub
@@ -22,8 +24,6 @@ Enable Automatic Deployment
 Deploy Branch
 
 go to setting tab
-
-callBackURL =
 
 Go to Technical Tab of APP
 Auth Callback URL
--- bigCommerce/settings.py
+++ bigCommerce/settings.py
@@ -85,16 +85,16 @@ X_FRAME_OPTIONS = 'ALLOWALL'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': config('DB_NAME'),
+        # 'USER': config('DB_USER'),
+        # 'PASSWORD': config('DB_PASSWORD'),
+        # 'HOST': config('DB_HOST'),
+        # 'PORT': '',
 
 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
