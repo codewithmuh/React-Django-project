c227578473b348195856bb726ce070c8a2d76147
eagle7 <khizar_siddique@yahoo.com>
2020-12-04 15:49:29 +0500

    F

--- callback/admin.py
+++ callback/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import *
-admin.site.register(Auth)
+# # Register your models here.
+# from .models import *
+# admin.site.register(Auth)
