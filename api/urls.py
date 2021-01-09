fbce5ef158684562674505b633365fbc5c8b69bf
eagle7 <khizar_siddique@yahoo.com>
2021-01-09 17:30:43 +0500

    G

--- api/urls.py
+++ api/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from api.views import orders, order, resources, resource, store
 
 urlpatterns = [
-    path('v2/orders', orders, name="ordersapi"),
+    path('v2/orders/', orders, name="ordersapi"),
         
-    path('v2/orders/<int:id>', order, name="orderapi"),
+    path('v2/orders/<int:id>/', order, name="orderapi"),
     path('', resources, name="orderapi"),
     
-    path('<int:id>', resource, name="orderapi"),
+    path('<int:id>/', resource, name="orderapi"),
 
 
-    path('v2/store', store, name="orderapi"),
-    path('v3/catalog/summary', resources, name="orderapi"),
+    path('v2/store/', store, name="orderapi"),
+    path('v3/catalog/summary/', resources, name="orderapi"),
 
 ]
